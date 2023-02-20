eaef5cb397c4f131cfc9cf551ee065150d7a1d0f
Muhammad Usman Baber <112895478+UsmanKamboh0@users.noreply.github.com>
2023-02-20 11:28:51 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,18 +3,12 @@ import cors from 'cors';
 import morgan from 'morgan';
 import path from 'path';
 import { dirname } from 'path';
-
-
 import config from './config.js'
 import BuyerRoutes from './routes/buyer.routes.js';
-// import OrderRoutes from './routes/order.routes';
 import SellerRoutes from './routes/seller.routes.js';
-// import BookRoutes from './routes/book.routes';
 import SellersRoutes from './routes/sellers.routes.js';
 import AuthRoutes from './routes/auth.routes.js';
-
 import { createRoles } from './libs/initialSetup.js';
-
 const app = express();
 createRoles()
 app.set('port', config.port)
@@ -27,36 +21,17 @@ app.use(cors(corsOptions))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-//routes
-// app.get('/', (req, res) => {
-//   res.json({ message: "book-fair-server..." })
-//  }
-//  )
  const __filename =("./build")
 const __dirname = dirname(__filename);
-
 app.use(express.static(path.join(__dirname, "./dist")));
-
-
-
-
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./dist/index.js"));
 });
-
-
-
 app.use('/api/seller/', SellerRoutes)
 app.use('/api/buyer/', BuyerRoutes)
 app.use('/api/sellers/', SellersRoutes)
 app.use('/api/auth', AuthRoutes)
-
-
-
-
-
 export default app;
