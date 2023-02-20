b019296eb7484dc99e3b75d9d7ac12542c5a0d42
Muhammad Usman Baber <112895478+UsmanKamboh0@users.noreply.github.com>
2023-02-20 10:49:47 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv'
 config()
 export default {
  secret: process.env.SECRET,
- mongodbURL: process.env.MONGODB_URI,
- port: process.env.PORT,
+ mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://usman:Usman12345@easyshopping.x8xdn9x.mongodb.net/Book-Fair?retryWrites=true&w=majority',
+ port: process.env.PORT || 8000,
  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000'
 }
